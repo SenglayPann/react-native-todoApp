a59b59fbf8621c8af26d86fa332a352534b5ed78
Senglay Pann <senglaypann@gmail.com>
2025-03-21 14:39:23 +0700

    Refactor: refactor home screen more dynamic

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {View} from '@gluestack-ui/themed';
 import TodoList from './TodoList';
-import todoMockData from '../mockup/todo';
 import {SharedTabRouteProp} from '../navigation/topTabsNavigation';
 import {conditionTodoList} from '../utils/filterTools';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux/store/store';
 const safeM = '$5';
 
 const Home = ({route}: {route: SharedTabRouteProp}) => {
+  const todoData = useSelector((state: RootState) => state.todoList);
   const context = route.name;
-  const todoData = conditionTodoList(todoMockData, context);
+  const filteredTodoData = conditionTodoList(todoData, context);
   return (
     <View mx={safeM}>
-      <TodoList todoList={todoData} />
+      <TodoList todoList={filteredTodoData} context={context} />
     </View>
   );
 };
--- src/screens/TodoList.tsx
+++ src/screens/TodoList.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import {FlatList} from '@gluestack-ui/themed';
+import React, {Fragment} from 'react';
+import {FlatList, View, Text} from '@gluestack-ui/themed';
 import TodoCard from '../components/TodoCard';
 import {Todo, TodoList as TypeTodoList} from '../types/todo';
 
 type Props = {
   todoList: TypeTodoList;
+  context: string;
 };
 
-const TodoList = ({todoList}: Props) => {
+const TodoList = ({todoList, context}: Props) => {
   return (
-    <FlatList
-      h={'$full'}
-      data={todoList}
-      renderItem={({item}) => <TodoCard todo={item} />}
-      keyExtractor={(todo: Todo) => todo.id}
-    />
+    <Fragment>
+      {!!todoList.length ? (
+        <FlatList
+          h={'$full'}
+          data={todoList}
+          renderItem={({item}) => <TodoCard todo={item} />}
+          keyExtractor={(todo: Todo) => todo.id}
+        />
+      ) : (
+        <View h={'$full'} alignItems="center" justifyContent="center">
+          <Text>
+            {`There is no ${context === 'All' ? '' : context} todo yet.`}
+          </Text>
+        </View>
+      )}
+    </Fragment>
   );
 };
 
